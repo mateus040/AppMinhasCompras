d9707f1502bb7fdbd0f437df966bad6b842b340f
mateus040 <mateusgabrielmorno264@gmail.com>
2022-11-21 09:01:08 -0300

    removendo MainPage

--- AppMinhasCompras/AppMinhasCompras/App.xaml.cs
+++ AppMinhasCompras/AppMinhasCompras/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppMinhasCompras.Helper;
+using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,11 +8,31 @@ namespace AppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper database;
+
+        public static SQLiteDatabaseHelper Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "arquivo.db3"
+                    );
+
+                    database = new SQLiteDatabaseHelper(path);
+                }
+
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new View.Listagem());
         }
 
         protected override void OnStart()
