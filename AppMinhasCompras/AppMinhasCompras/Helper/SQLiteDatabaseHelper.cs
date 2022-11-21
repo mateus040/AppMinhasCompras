e64b2faf4df1d227731ce486fe3bd215264beb70
Usuario <Usuario@CPD03-M10>
2022-11-21 08:49:16 -0300

    Adicionar arquivos de projeto.

---
+++ AppMinhasCompras/AppMinhasCompras/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppMinhasCompras.Helper
+{
+    internal class SQLiteDatabaseHelper
+    {
+    }
+}
---
+++ AppMinhasCompras/AppMinhasCompras/Model/Produto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppMinhasCompras.Model
+{
+    internal class Produto
+    {
+    }
+}
